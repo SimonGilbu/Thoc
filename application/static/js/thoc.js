24d1ee06f5c7f9d92cca2650652da2e2c02cff14
human <mail@gilbu.dev>
2024-03-03 19:43:46 +0100

    simple dev test

--- application/static/js/thoc.js
+++ application/static/js/thoc.js
@@ -1,18 +1,14 @@
-Office.onReady(() => {
-   addSelectionChangedEventHandler();
-})
-
-
+Office.onReady(function () {
+    addSelectionChangedEventHandler();
+});
 function addSelectionChangedEventHandler() {
     Office.context.document.addHandlerAsync(Office.EventType.DocumentSelectionChanged, MyHandler);
 }
-
-function MyHandler(eventArgs: Office.DocumentSelectionChangedEventArgs) {
+function MyHandler(eventArgs) {
     setMessage('Event raised: ' + eventArgs.type);
 }
-
-function setMessage(text: string) {
-    const msgBox = document.getElementById("activeSelectionName");
+function setMessage(text) {
+    var msgBox = document.getElementById("activeSelectionName");
     if (msgBox) {
         msgBox.innerHTML = text;
     }
